1e6e92d6cc9e1ef0af9aa916547ef6843e851a33
jeevankrishna-2006 <puppalajeevankrishna2006@gmail.com>
2025-09-06 12:33:07 +0530

    third

--- practice.js
+++ practice.js
@@ -17,5 +17,22 @@ let x;
 console.log(x)
 let y=null;
 console.log(y)
+console.log(typeof x)
+console.log(typeof y)
+let arr=[1,1,2,3,4,45,];
+console.log(typeof arr)
+console.log(arr[0])
+let person={
+    name:"jk",
+    age:18
+}
+console.log(person.name)
+
+console.log(person.age)
+console.log(typeof person)
+
+
+
+
 
 
