ca6e14429568a97790c66a82e434139fbfd26845
jeevankrishna-2006 <puppalajeevankrishna2006@gmail.com>
2025-09-06 12:28:41 +0530

    third

--- practice.js
+++ practice.js
@@ -3,13 +3,19 @@ console.log("hello world")
 let name="Jeevan";
 console.log(name)
 
-let num =18
+let num =18;
 console.log(num)
 
-let islogged =false
+let islogged =false;
 console.log( islogged)
 
 
 console.log(typeof name)
 console.log(typeof num)
 console.log(typeof islogged)
+let x;
+console.log(x)
+let y=null;
+console.log(y)
+
+
