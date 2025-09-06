5e25540f49c218ad1d61eddda4486973c383202d
jeevankrishna-2006 <puppalajeevankrishna2006@gmail.com>
2025-09-06 12:26:31 +0530

    third

--- practice.js
+++ practice.js
@@ -5,3 +5,11 @@ console.log(name)
 
 let num =18
 console.log(num)
+
+let islogged =false
+console.log( islogged)
+
+
+console.log(typeof name)
+console.log(typeof num)
+console.log(typeof islogged)
