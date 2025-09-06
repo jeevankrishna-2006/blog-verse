f7c02abca5a8becfd76f8936738248c1b6860a1d
jeevankrishna-2006 <puppalajeevankrishna2006@gmail.com>
2025-09-06 16:50:42 +0530

    math

---
+++ main.js
@@ -0,0 +1,7 @@
+import {add,mul,sub,div} from "./math.js"
+import pow from "./math.js"
+console.log(add(2,3))
+console.log(mul(118,2))
+console.log(sub(240,4))
+console.log(div(4,2))
+console.log(pow(2,2))
---
+++ math.js
@@ -0,0 +1,17 @@
+export const mul=function(x,y){
+    return x*y;
+}
+export const add=function(x,y){
+    return x+y;
+}
+export const sub=function(x,y){
+    return x-y;
+}
+export const div=function(x,y){
+    return x/y;
+}
+
+const pow=function(x,y){
+    return x**y;
+}
+export default pow;
