76f2768b2e0e2c3ba36184445fdff05025a057db
jeevankrishna-2006 <puppalajeevankrishna2006@gmail.com>
2025-09-06 15:15:02 +0530

    operators

---
+++ controlflow.js
@@ -0,0 +1,45 @@
+let marks=95;
+if (marks>=90){
+    console.log("A+ grade")
+}
+else if (marks>=80){
+    console.log("A grade")
+}
+else if (marks>=70){
+    console.log("B grade")
+}
+else if (marks>=60){
+    console.log("C grade")
+}
+else{
+    console.log("fail")
+}
+let ch=5;
+switch(ch)
+{
+    case 1:
+        console.log("sunday")  
+        break;  
+    case 2:
+        console.log("monday")
+        break;
+    case 3:
+        console.log("tuesday")
+        break;
+    case 4:
+        console.log("wednesday")  
+        break;  
+    case 5:
+        console.log("thursday")
+        break;
+    case 6:
+        console.log("friday")
+        break;
+    case 7:
+        console.log("saturday")
+        break;
+    default:
+        console.log("Invalid")
+        break;s
+}
+    
